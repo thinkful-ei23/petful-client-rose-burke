a10a1d298bfdcc6faa1f74202a577f84c02b2d45
Burkey Koontz <bk.koontz@gmail.com>
2018-10-12 09:43:13 -0700

    working somewhat dynamically

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import ToAdopt from './ToAdopt';
 
 export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      cats : {
+        imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+        imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+        name: 'Fluffy',
+        sex: 'Female',
+        age: 2,
+        breed: 'Bengal',
+        story: 'Thrown on the street'
+      },
+      dogs : {
+        imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+        imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+        name: 'Zeus',
+        sex: 'Male',
+        age: 3,
+        breed: 'Golden Retriever',
+        story: 'Owner Passed away'
+      }
     }
   }
 
   render() {
     return (
       <div className="parent">
-      <section className="catToAdopt">
-      <toAdopt />
-      </section >
-      
-      <section className="dogToAdopt">
-      <toAdopt /> 
-      </section>
-      
+        <section className="catToAdopt">
+          <ToAdopt animal={this.state.dogs}/>
+        </section >
+        <section className="dogToAdopt">
+          <ToAdopt animal={this.state.cats} /> 
+        </section>
       </div> 
-    )
+    );
   }
 }
--- src/toAdopt.js
+++ src/toAdopt.js
@@ -1,33 +1,35 @@
 import React from 'react';
 
-export default function toAdopt() {
+export default function ToAdopt(props) {
+
+  //imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  // imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  // name: 'Fluffy',
+  // sex: 'Female',
+  // age: 2,
+  // breed: 'Bengal',
+  // story: 'Thrown on the street'
   return (
   <div>
-  <header className="animalName"> 
-  <h1> 
-    animal name
-  </h1>
-  <img />
-  </header>
-
-
-  <main> 
-    <dl>
-      <dt>Name</dt>
-      <dd>Fluffy</dd>
-      <dt>Sex</dt>
-      <dd>Female</dd>
-      <dt>Age</dt>
-      <dd>2</dd>
-      <dt>Breed</dt>
-      <dd>Bengal</dd>
-      <dt>Story</dt>
-      <dd>Thrown out on the street</dd>
-    </dl>
-    <button>Adopt</button>
-  </main>
-
+    <header className="animalName"> 
+      <h1> 
+       {props.animal.name}
+      </h1>
+      <img src={props.animal.imageUrl} alt={props.animal.imageDescription}/>
+    </header>
+    <main> 
+      <dl>
+        <dt>Sex</dt>
+        <dd>{props.animal.sex}</dd>
+        <dt>Age</dt>
+        <dd>{props.animal.age}</dd>
+        <dt>Breed</dt>
+        <dd>{props.animal.breed}</dd>
+        <dt>Story</dt>
+        <dd>{props.animal.story}</dd>
+      </dl>
+      <button>Adopt</button>
+    </main>
   </div>
-
   );
 }
