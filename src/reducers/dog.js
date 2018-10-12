56a65dd330693c3eb2e30527c0111662d9caa97c
Rose Sorfa <rose.otter.2015@gmail.com>
2018-10-12 13:50:22 -0500

    added reducers

--- src/reducers/dog.js
+++ src/reducers/dog.js
@@ -7,12 +7,20 @@ const initialState = {
 }
 
 export const dogReducer = (state=initialState, action) => {
-  if(action.type === actions.ADOPT_DOG_REQUEST) {
-    return Object.assign({}, state, {
-      dogs: [...state.dogs]
-    });
-  }
-  else {
-    return state;
-  }
+  switch (action.type) { 
+    case actions.FETCH_DOG_REQUEST: 
+      return {...state, loading : true};
+    case actions.FETCH_DOG_SUCCESS: 
+      return {...state, loading : false, data: action.dog};  
+    case actions.FETCH_DOG_ERROR: 
+      return {...state, loading : false, error: action.error};
+    case actions.ADOPT_DOG_REQUEST: 
+      return {...state, loading : true};
+    case actions.ADOPT_DOG_SUCCESS: 
+      return {...state, loading : false};  
+    case actions.ADOPT_DOG_ERROR: 
+      return {...state, loading : false, error: action.error};
+      default: 
+      return state; 
+  }     
 }; 
